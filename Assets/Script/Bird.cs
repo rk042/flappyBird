24b303233daaf2dbb0c07b7948ddc915d1a884b3
ketan <rathodketan042@gmail.com>
2020-10-18 12:45:45 +0530

    Move Bird and follow the camera bird and use onclick button using code
    create new script call cameraFollow and atteched in main camera , also use onclick button using code

--- Assets/Script/Bird.cs
+++ Assets/Script/Bird.cs
@@ -1,18 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Bird : MonoBehaviour
 {
+    public static Bird instance;
+
+    private Rigidbody2D myRigidbody;
+
+    private Animator anim;
+
+    private float forwordSpeed=3f;
+    private float bounceSpeed=4f;
+
+    private Button FlapButton;
+    private bool didFlap;
+    public bool isAlive;
+    private void Awake()
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        FlapButton = GameObject.FindGameObjectWithTag("FlapButton").GetComponent<Button>();
+
+        FlapButton.onClick.AddListener(FlapTheBird);
+
+        if(instance == null)
+        {
+            instance = this; 
+        }
+
+        isAlive = true;
+
+        setCameraX();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(isAlive)
+        {  
+           // print(transform.position);
+            Vector3 temp = transform.position;
+            temp.x += forwordSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            //print(Time.timeScale);
+           // print(myRigidbody.velocity.y);
+           // print(temp.x);
+            if(didFlap)
+            {
+                didFlap = false;
+                myRigidbody.velocity = new Vector2(0,bounceSpeed);
+                anim.SetTrigger("Flap");
+            }
+
+            if(myRigidbody.velocity.y >=0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                float angle = 0;
+                angle = Mathf.Lerp(0, -90, -myRigidbody.velocity.y / 7);
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+
+        }
+    }
+
+    void setCameraX()
+    {
+        CameraFollow.offsetX = (Camera.main.transform.position.x - transform.position.x) - 1f;
+    }
+
+    public float GetPositionX()
+    {
+        return transform.position.x;
+    }
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
     }
 }
---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public static float offsetX;
+
+    private void Update()
+    {
+        if(Bird.instance !=null)
+        {
+            if(Bird.instance.isAlive)
+            {
+                moveCamera();
+            }
+        }
+    }
+    public void moveCamera()
+    {
+        Vector3 temp = transform.position;
+        temp.x = Bird.instance.GetPositionX()+ offsetX;
+        transform.position = temp;
+    }
+}
